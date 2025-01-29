6bca12d68828c8ad079586e2cd18c632a0764f83
Harshit Jain <iharshitjain99@gmail.com>
2025-01-29 21:19:27 +0530

    backend initiated

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//api endpoint
+app.get("/",(req,res)=>{
+  res.send("working");
+})
+
+app.listen(port,()=>{
+  console.log("server is running on:",port);
+})
