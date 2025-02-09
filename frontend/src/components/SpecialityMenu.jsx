0b195cabd5b195e8484916d4934d994ba487127d
megha <meghagupta270505@gmail.com>
2025-02-09 16:53:37 +0530

    adding css

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -10,7 +10,7 @@ const SpecialityMenu = () => {
           <p className='sm:w-1/3 text-center text-sm'>Simply browse through extensie list of trusted doctors,schedule your menu hassle-free</p>
           <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
               {specialityData.map((item,index) => (
-                  <Link className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
+                  <Link  onClick={() => scrollTo(0, 0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
                       <img  className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
                       <p>{item.speciality}</p>
                       
