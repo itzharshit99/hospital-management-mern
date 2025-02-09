8c010ae813fa3dc6644cf0c34760280a577dcb17
megha <meghagupta270505@gmail.com>
2025-02-09 22:40:56 +0530

    adding css

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { doctors } from "../assets/assets";
+import React, { useContext } from "react";
+//import { doctors } from "../assets/assets";
+import {useNavigate} from 'react-router-dom'
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  const {doctors}=useContext(AppContext);
+
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -13,7 +18,7 @@ const TopDoctors = () => {
         style={{ gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))" }}
       >
         {doctors.slice(0, 10).map((item, index) => (
-          <div
+          <div onClick={()=>navigate(`/appointment/${item._id}`)}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all divide-purple-500"
             key={index}
           >
@@ -23,13 +28,13 @@ const TopDoctors = () => {
                 <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                 <p>Available</p>
               </div>
-              <p>{item.name}</p>
-              <p>{item.speciality}</p>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-600 text-sm">{item.speciality}</p>
             </div>
           </div>
         ))}
       </div>
-      <button>more</button>
+      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">more</button>
     </div>
   );
 };
