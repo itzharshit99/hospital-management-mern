e7c02a1bd033e17da844ebf57ab750d505909b1f
megha <meghagupta270505@gmail.com>
2025-02-12 22:42:27 +0530

    adding doc pages

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Appointments = () => {
   const { docId } = useParams();
@@ -17,8 +18,24 @@ const Appointments = () => {
     fetchDocInfo();
   }, [doctors, docId]);
 
-  return (
-    <div>
+  return docInfo &&(
+    <div >
+      <div className="flex flex-col sm:flex-row gap-4">
+        <div>
+          <img  className="bg-blue-500 w-full sm:max-w-72 rounded-lg" src={docInfo.image} alt="" />
+        </div>
+        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+          <p>{docInfo.name} <img src={assets.verified_icon} alt="" /></p>
+          <div>
+            <p>{docInfo.degree}-{docInfo.speciality}</p>
+            <button>{docInfo.experience}</button>
+          </div>
+          <div>
+            <p>About <img src={assets.info_icon} alt="" /></p>
+            <p>{docInfo.about}</p>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
