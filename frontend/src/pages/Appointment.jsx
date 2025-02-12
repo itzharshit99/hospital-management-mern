6543cac8d9eb63c256567215a527d03c019e4ace
megha <meghagupta270505@gmail.com>
2025-02-12 18:55:59 +0530

    adding doc routes

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import {useParams} from "react-router-dom";
-import {AppContext} from "../context/AppContext"
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Appointments = () => {
-  const {docId}=useParams();
-  const {doctors}=useContext(AppContext);
-  return <div>
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
 
-  
-   new Appointments</div>;
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default Appointments;
