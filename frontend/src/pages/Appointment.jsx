e0979c07df81260e8285185250c3e6c30ae837c5
megha <meghagupta270505@gmail.com>
2025-02-15 14:41:52 +0530

    adding time slot

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -3,70 +3,117 @@ import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 
-const Appointments = () => {
+const Appointment = () => {
   const { docId } = useParams();
-  const { doctors,currencySymbol } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const [docInfo, setDocInfo] = useState(null);
   const [docSlots, setDocSlots] = useState([]);
   const [slotIndex, setSlotIndex] = useState(0);
-  const [slotTime, setSlotTime] = useState('');
+  const [slotTime, setSlotTime] = useState("");
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
-    //console.log(docInfo);
   };
 
   const getAvailableSlots = async () => {
-    setDocSlots([])
+    setDocSlots([]);
 
     let today = new Date();
-    for (let i = 0; i < 7; i++){
+    for (let i = 0; i < 7; i++) {
       let currentDate = new Date(today);
       currentDate.setDate(today.getDate() + i);
-      let endTime=new Date()
-      endTime.setDate(today.getDate() + 1)
-      endTime.setHours(21,0,0,0)
-      if(today.getDate()===currentDate.getDate()){
-        //currentDate.setHours
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10,
+          0
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10, 0);
+        currentDate.setMinutes(0);
+      }
+      let timeSlots = [];
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        timeSlots.push({
+          dateTime: new Date(currentDate),
+          time: formattedTime,
+        });
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+      setDocSlots((prev) => [...prev, timeSlots]);
     }
-    
-  }
+  };
 
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
 
-  useEffect(()=>{
-    getAvailableSlots()
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
 
-  },[docInfo])
+  useEffect(() => {
+    console.log(docSlots);
+  }, [docSlots]);
 
-  return docInfo &&(
-    <div >
-      <div className="flex flex-col sm:flex-row gap-4">
-        <div>
-          <img  className="bg-[#5f6FFF] w-full sm:max-w-72 rounded-lg" src={docInfo.image} alt="" />
-        </div>
-        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
-          <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">{docInfo.name}
-            <img  className="w-5" src={assets.verified_icon} alt="" /></p>
-          <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
-            <p>{docInfo.degree}-{docInfo.speciality}</p>
-            <button className="py-0.5 px-2 border text-xs rounded-full">{docInfo.experience}</button>
-          </div>
+  return (
+    docInfo && (
+      <div>
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">About <img src={assets.info_icon} alt="" /></p>
-            <p className="text-sm text-gray-500 max-w-[700px] mt-1">{docInfo.about}</p>
+            <img
+              className="bg-[#5f6FFF] w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
           </div>
-          <p className="text-gray-500 font-medium mt-4">
-            Appointment fee: <span className="text-gray-600">{currencySymbol}{docInfo.fees}</span>
-          </p>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}
+              <img className="w-5" src={assets.verified_icon} alt="" />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree}-{docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment fee:{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+          <p>Booking slots</p>
+          <div>{docSlots.length &&}</div>
+
         </div>
       </div>
-      
-    </div>
+    )
   );
 };
 
-export default Appointments;
+export default Appointment;
