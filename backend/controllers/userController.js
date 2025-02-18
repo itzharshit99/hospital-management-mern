b5e51407b48228cf43c16925248461e843b87346
Harshit Jain <iharshitjain99@gmail.com>
2025-02-18 17:59:54 +0530

    update profile route done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ import validator from 'validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/usermodel.js'; // Add this import
+import {v2 as cloudinary} from 'cloudinary'
 
 const registerUser=async(req,res)=>{
     try {
@@ -74,7 +75,19 @@ const getProfile = async (req,res)=>{
 
 const updateProfile = async (req,res)=>{
     try {
-        
+        const {userId,name,phone,address,dob,gender} = req.body;
+        const imageFile = req.file
+        if(!name || !phone || !dob || !gender){
+            return res.json({success:false,message:"Data Missing"})
+        }
+        await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender});
+        if(imageFile){
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+            const imageURL = imageUpload.secure_url
+            await userModel.findByIdAndUpdate(userId,{image:imageURL})
+            
+        }
+        res.json({success:true,message:"profile updated"})
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
@@ -83,4 +96,4 @@ const updateProfile = async (req,res)=>{
 
 
 
-export {registerUser,loginUser,getProfile};
+export {registerUser,loginUser,getProfile,updateProfile};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { registerUser,loginUser, getProfile } from '../controllers/userController.js';
+import { registerUser,loginUser, getProfile, updateProfile } from '../controllers/userController.js';
 import authUser from '../middleware/auth.user.js';
+import upload from '../middleware/multer.js';
 const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 
 userRouter.post("/login", loginUser);
 userRouter.get("/get-profile",authUser,getProfile);
+userRouter.post("/update-profile",upload.single('image'),authUser,updateProfile)
 
 
 export default userRouter;
