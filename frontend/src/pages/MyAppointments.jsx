bc49b499716c42d696c63d8a0e6af7e3403115f4
Harshit Jain <iharshitjain99@gmail.com>
2025-02-20 17:42:12 +0530

    all appointments rendered on frontend successfully

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,37 +1,88 @@
-import React,{useContext} from 'react'
-import {AppContext} from '../context/AppContext'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const MyAppointments = () => {
-
-const {doctors} = useContext(AppContext)
+  const { backendUrl, token } = useContext(AppContext);
+  const [appointments, setAppointments] = useState([]);
+  const months = ["",
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+  ]
+  const slotDateFormate = (slotDate)=>{
+    const dateArray = slotDate.split('_');
+    return dateArray[0] + " " + months[Number(dateArray[1])]+  " " + dateArray[2]
+  }
+  const getUserAppointments = async (req, res) => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/appointments", {
+        headers: { token },
+      });
+      if (data.success) {
+        setAppointments(data.appointments.reverse());
+        console.log(data.appointments);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (token) {
+      getUserAppointments();
+    }
+  }, [token]);
 
   return (
     <div>
-      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
+        My appointments
+      </p>
       <div>
-        {doctors.slice(0,3).map((item,index)=>(
-          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+        {appointments.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
+            key={index}
+          >
             <div>
-              <img className='w-32 bg-indigo-50' src={item.image} alt='' />
-              </div>
-                <div className='flex-1 text-sm text-zinc-600'>
-                  <p className='text-neutral-800-800 font-semibold'>{item.name}</p>
-                  <p>{item.speciality}</p>
-                  <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-                  <p className='text-sx'>{item.address.line1}</p>
-                  <p className='text-sx'>{item.address.line2}</p>
-                  <p className='text-sx mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> 25, July, 2024</p>
-                </div>
-                <div></div>
-                <div className='flex flex-col gap-2 justify-end'>
-                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-[#5f6FFF] hover:text-white transition-all duration-300'>Pay Online</button>
-                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border  hover:bg-[#5f6FFF] hover:text-white transition-all duration-300'>Cancel appointment</button>
-                </div>
-              </div>
+              <img
+                className="w-32 bg-indigo-50"
+                src={item.docData.image}
+                alt=""
+              />
+            </div>
+            <div className="flex-1 text-sm text-zinc-600">
+              <p className="text-neutral-800-800 font-semibold">
+                {item.docData.name}
+              </p>
+              <p>{item.docData.speciality}</p>
+              <p className="text-zinc-700 font-medium mt-1">Address:</p>
+              <p className="text-sx">{item.docData.address.line1}</p>
+              <p className="text-sx">{item.docData.address.line2}</p>
+              <p className="text-sx mt-1">
+                <span className="text-sm text-neutral-700 font-medium">
+                  Date & Time:
+                </span>{slotDateFormate(item.slotDate)}
+                | {item.slotTime}
+              </p>
+            </div>
+            <div></div>
+            <div className="flex flex-col gap-2 justify-end">
+              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-[#5f6FFF] hover:text-white transition-all duration-300">
+                Pay Online
+              </button>
+              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border  hover:bg-red-600 hover:text-white transition-all duration-300">
+                Cancel appointment
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyAppointments
+export default MyAppointments;
